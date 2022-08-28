32f445c8b1b16fea4ad96e5a3b35ab7d7421e777
Swapnil Parchake <swapnilparchakewrd2@gmail.com>
2022-08-28 16:41:28 +0530

    bottom footer completed

--- src/Pages/Scorepage/Scorecard.jsx
+++ src/Pages/Scorepage/Scorecard.jsx
@@ -2,6 +2,7 @@ import { Box } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
+import { BottomFooter } from '../../Components/BottomFooter'
 import Navbar from '../../Components/Navbar'
 import { SchedualFilter } from '../../Components/Schedual/SchedualFilter'
 import MatchDetail from '../../Components/ScorePage/MatchDetail'
@@ -14,7 +15,9 @@ export const Scorecard = () => {
     <SchedualFilter/>
     <Navbar/>
     <MatchDetail/>
-
+    <footer>
+      <BottomFooter/>
+    </footer>
 </Box>
     </div>
   )
